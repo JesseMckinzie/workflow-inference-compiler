ecce7aecf80a0902ddfa8163bf6f01dc15551e54
JesseMckinzie <jessemckinzie145@gmail.com>
2024-09-10 11:53:53 -0600

    Add itc spec

---
+++ src/sophios/api/utils/ict/ict_spec/hardware/__init__.py
@@ -0,0 +1,5 @@
+"""Hardware Requirements for ICT."""
+
+from sophios.api.utils.ict.ict_spec.hardware.objects import CPU, GPU, HardwareRequirements, Memory
+
+__all__ = ["CPU", "Memory", "GPU", "HardwareRequirements"]
---
+++ src/sophios/api/utils/ict/ict_spec/validate.py
@@ -0,0 +1,48 @@
+import json
+from functools import singledispatch
+from pathlib import Path
+from typing import Any
+
+from yaml import safe_load
+
+from sophios.api.utils.ict.ict_spec.model import ICT
+
+
+@singledispatch
+def validate(file: Any) -> ICT:
+    """Validate an ICT specification."""
+    raise NotImplementedError(f"File type not supported: {file}")
+
+
+@validate.register  # no Union[Path, str] <3.11
+def _(file: str) -> ICT:
+    """Validate an ICT specification."""
+    if str(file).endswith(".yaml") or str(file).endswith(".yml"):
+        with open(file, "r", encoding="utf-8") as f_o:
+            data = safe_load(f_o)
+    elif str(file).endswith(".json"):
+        with open(file, "r", encoding="utf-8") as f_o:
+            data = json.load(f_o)
+    else:
+        raise ValueError(f"File extension not supported: {file}")
+    return validate(data)
+
+
+@validate.register  # no Union[Path, str] <3.11
+def _(file: Path) -> ICT:
+    """Validate an ICT specification."""
+    if str(file).endswith(".yaml") or str(file).endswith(".yml"):
+        with open(file, "r", encoding="utf-8") as f_o:
+            data = safe_load(f_o)
+    elif str(file).endswith(".json"):
+        with open(file, "r", encoding="utf-8") as f_o:
+            data = json.load(f_o)
+    else:
+        raise ValueError(f"File extension not supported: {file}")
+    return validate(data)
+
+
+@validate.register
+def _(ict: dict) -> ICT:
+    """Validate an ICT specification."""
+    return ICT(**ict)
